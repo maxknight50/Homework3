0aca28918dcb190beb3c762127bf4914a29867a0
maxknight50 <maxknight50@gmail.com>
2022-06-15 20:11:42 -0400

    Added pop up

--- src/homework3/Homework3.java
+++ src/homework3/Homework3.java
@@ -3,14 +3,14 @@ package homework3;
 import java.io.Serializable;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
- * Maxine Knight, Zachary Taylor 
- * Homework 3
+ * Maxine Knight, Zachary Taylor Homework 3
  */
 public class Homework3 extends Application implements Serializable {
 
@@ -29,9 +29,15 @@ public class Homework3 extends Application implements Serializable {
     Button raise = new Button("Raise");
     Button lower = new Button("Lower");
     Button view = new Button("View Item Detail");
+    
+    // For Edit Categories
+    TextField catTxt = new TextField();
+    Button catAdd = new Button("Add");
+    Button delete = new Button("Delete Selected");
+
 
     // Create GridPanes
-    GridPane overallPane = new GridPane(); 
+    GridPane overallPane = new GridPane();
     GridPane listControl = new GridPane();
     GridPane buttonPane = new GridPane();
     GridPane newPane = new GridPane();
@@ -77,9 +83,34 @@ public class Homework3 extends Application implements Serializable {
         primaryStage.setTitle("ToDo");
         primaryStage.show();
 
+        addButton.setOnAction(e -> {
+            SmallWindow();
+        });
     }
 
-    public static void main(String[] args) {
+    ///////////
+    public void SmallWindow() {        
+
+        Stage primaryStage = new Stage();
+        GridPane primaryPane = new GridPane();
+
+        primaryPane.add(catTxt, 0, 0);
+        primaryPane.add(catAdd, 0, 1);
+        primaryPane.add(delete, 0, 2);
+        primaryPane.setAlignment(Pos.CENTER);
+
+        Scene primaryScene = new Scene(primaryPane, 400, 300);
+        primaryStage.setScene(primaryScene);
+        primaryStage.setTitle("Edit Categories");
+        primaryStage.show();
+
+//        btnSendBack.setOnAction(e -> {
+//            
+//        });
+    }
+
+
+public static void main(String[] args) {
         launch(args);
     }
 
